30ae6d700d85adb0502779714333d90f762ab7b6
Mathew-Wokabi <wokabimathew@gmail.com>
2024-03-12 11:31:32 +0300

    update

--- Python_lessons.py
+++ Python_lessons.py
@@ -1,1 +1,36 @@
-gi
+#python comments-this is a single line comment
+"""
+this is a multiline comment in python
+"""
+
+#variables
+student_name ="Mathew"  #data type is string
+student_age = 20    #data type is interger
+student_fee= 100.0   #data type is float
+student_mark= 100   #data type is boolean
+is_male=True
+
+#outputting the values in the variables
+print(student_name)
+print(student_age)
+print(is_male)
+
+
+student_name="Mathew"
+student_name="Allan"
+
+print(student_name)
+
+
+STUDENT_NAME="Ryan"
+student_name="James"
+print(student_name)
+
+#variable naming in python
+book_price=20.0 #valid variable name
+_book_price=20.0 #valid variable name
+*book_price=10.0
+123_book_price=0
+book_price_123=40
+
+
